7e039d2e2fa8a3d849fb1b6caee35b427c7c1b08
thanh12-dtu-test <62973821+thanh12-dtu-test@users.noreply.github.com>
2022-03-02 06:21:47 +0700

    Update FinalTest-TrafficLight-NgocThanh.cpp

--- FinalTest-TrafficLight-NgocThanh.cpp
+++ FinalTest-TrafficLight-NgocThanh.cpp
@@ -1,135 +1,51 @@
-#include <iostream>
-#include <fstream>
-#include <ctime>
-#include <string>
-#include <vector>
-#include <sstream>
-#include <algorithm>
-#include <iomanip>
-using namespace std;
-
-// class TrafficLight {
-// public:
-//     void changeState()
-// }
-
-class ILightState {
-public:
-    // LightState(LightColor init_state);
-    // LightColor getState const;
-    // void wait() const;
-    // void nextState();
-    virtual void changeLightState() =0;
-};
-
-class TrafficLight {
-    private:
-    ILightState* m_state;
-public:
-    // TrafficLight(){
-    //     m_state = new RedLight(this)
-    // }
-    TrafficLight();
-    void setState(ILightState* state);
-    void handle();
-};
-
-
-
 #include "FinalTest-TrafficLight-NgocThanh.hpp"
 
+TrafficLight::TrafficLight() {
+    m_state = new RedLight(this);
+}
+
 void TrafficLight::setState(ILightState* state) {
-		m_state = state;
-	}
-void TrafficLight::handle(){
-        m_state->changeLightState();
-    }
+	m_state = state;
+}
 
+//ILightState* TrafficLight::getState() {
+//	return m_state;
+//}
 
-class YellowLight : public ILightState {
-private:
-    TrafficLight* m_context;
-public:
-    RedLight(TrafficLight* context) : m_context(context) {}
-    void changeLightState() {
-        m_context->setState(new RedLight(m_context));
-    }
-    void display() {
-        cout<< "Yellow light" <<endl;
-    }
-};
+void TrafficLight::reportLight() {      
+	m_state->reportLightState();
+}
 
-class GreenLight : public ILightState {
-private:
-    TrafficLight* m_context;
-public:
-    GreenLight(TrafficLight* context): m_context(context) {}
-    void changeLightState() {
-        m_context->setState(new YellowLight(m_context));
-    }
-    void display() {
-        cout<< "Green light" <<endl;
-    }
+void TrafficLight::changeLight(){
+    m_state->changeLightState();
+}
+
+YellowLight::YellowLight(TrafficLight* context) : m_context(context) {}
+
+void YellowLight::changeLightState() {
+    m_context->setState(new RedLight(m_context));
+}
     
-};
-
-class RedLight : public ILightState {
-private:
-    TrafficLight* m_context;
-public:
-    RedLight(TrafficLight* context) : m_context(context) {}
-    void changeLightState() {
-        m_context->setState(new GreenLight(m_context));
-    }
-    void display() {
-        cout<< "Red light" <<endl;
-    }
-};
-
-map<string, int> time_config;
-
-void getConfigFromFile() {
-    std::ifstream inputFile;
-    inputFile.open("FinalTest-TimeConfig-NgocThanh.cfg", std::ios::in);
-    if(inputFile.is_open()) {
-        std::string line, word;
-        while(std::getline(inputFile, line)) {
-            std::vector<std::string> words;
-            std::istringstream iss(line);
-            while(iss >> word) {
-                words.push_back(word);
-            }
-            time_config.push_back(words[0], std::stoi(words[1]));
-        }
-        inputFile.close();
-        return EXIT_SUCCESS;
-    }
-    else {
-        std::cerr << "Could not open the file" << std::endl;
-        return EXIT_FAILURE;
-    }
+void YellowLight::reportLightState() {
+    cout<< "YELLOW light";
 }
 
+GreenLight::GreenLight(TrafficLight* context): m_context(context) {}
 
-void lauchTrafficLight() {
-    enum ILightState {
-        RedLight,
-        YellowLight,
-        GreenLight,
-    } *currentState = RedLight;
-    TrafficLight light;
-    light.m_state = currentState;
-    while(true) {
-        light.display();
-        light.changeLightState();
-    }
-
+void GreenLight::changeLightState() {
+    m_context->setState(new YellowLight(m_context));
 }
-int main() {
+    
+void GreenLight::reportLightState() {
+    cout<< "GREEN light";
+} 
 
-    lauchTrafficLight();
+RedLight::RedLight(TrafficLight* context) : m_context(context) {}
 
-    return 0;
+void RedLight::changeLightState() {
+    m_context->setState(new GreenLight(m_context));
 }
 
-#endif
+void RedLight::reportLightState() {
+    cout<< "RED light";
+}
